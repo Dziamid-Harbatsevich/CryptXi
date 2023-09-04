c6eda59631e3ab1a2d5e1f66e3423f2a64e414f2
Dziamid Harbatsevich <newads777@gmail.com>
2023-09-04 09:43:30 +0300

    add Console client

---
+++ CryptXiConsoleClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+using Interop.CryptXi;
+using System.Runtime.InteropServices;
+
+namespace CryptXiConsoleClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ComATLCryptXiObject = new ATLCryptXiObject();
+
+            string key = "Some test key";
+            string textPlain = "Some plain text";
+
+            string result = ComATLCryptXiObject.SetKey(key);
+            Console.WriteLine($"Client key: {result}");
+
+            string encryptedText = ComATLCryptXiObject.Encrypt(textPlain);
+            Console.WriteLine($"Client encryptedText: {encryptedText}");
+
+            string decryptedText = ComATLCryptXiObject.Decrypt(encryptedText);
+            Console.WriteLine($"Client decryptedText: {decryptedText}");
+
+
+            Console.ReadLine();
+
+            Marshal.ReleaseComObject(ComATLCryptXiObject);
+        }
+    }
+}
